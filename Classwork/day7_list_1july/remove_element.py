93ccfd8879f49814b6c1703cafa7c8c1ddd3b46e
Priyanshi-coder5 <priyanshi0931@gmail.com>
2026-07-01 12:18:49 +0530

    list using the remove() method

---
+++ Classwork/day7_list_1july/remove_element.py
@@ -0,0 +1,20 @@
+#----------problem statement---------
+# Program to remove an element from a list using the remove() method
+
+
+
+
+
+#-------------coding-----------------------
+numbers = [40, 50, 60, 70, 80, 90, 100, 110, 120, 130]
+print ("numbers:", numbers)
+print ("-----------------------------")
+#removing from list using index
+numbers.remove(90)
+print ("numbers after removing 90:", numbers)
+print("after removing 90:", numbers)
+#----------output-----------------------
+'''--------------------
+numbers are [40, 50, 60, 70, 80, 100, 110, 120, 130]
+after removing 90: [40, 50, 60, 70, 80, 100, 110, 120, 130]
+----------------------'''
