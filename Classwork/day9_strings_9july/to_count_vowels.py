ad1f7970e80a64d601b5058d7b14229ee20c3257
Priyanshi-coder5 <priyanshi0931@gmail.com>
2026-07-06 10:12:09 +0530

    count vowels in sentence

--- Classwork/day9_strings_9july/to_count_vowels.py
+++ Classwork/day9_strings_9july/to_count_vowels.py
@@ -1,37 +1,20 @@
 #----------------problem statement------------------
-'''Write a python program to count the number of vowels in a string.'''
+'''Write a python program to take a string from user and count the number of vowels in it.'''
+ #input of sentence 
+ sentence = input("Enter a sentence : ")
 
-
-#----------------coding----------------------
-# Step 1: Take input from user
-string = input("Enter a string: ")
-
-# Step 2: Initialize vowel count
+#initialize a variable to count vowels
 vowel_count = 0
-
-# Step 3: Define vowels
-vowels = "aeiouAEIOU"
-
-# Step 4: Count vowels
-for char in string:
-    if char in vowels:
+for x in sentence:
+    if (x=='A' or x=='a' or x=='E' or x=='e' or x=='I' or x=='i' or x=='O' or x=='o' or x=='U' or x=='u'):
         vowel_count += 1
+        vowel = vowel +1
 
-# Step 5: Display result
-print("Number of vowels in the string:", vowel_count)
+print("No of vowels in the given sentence is : ",vowel_count)
+ 
 
 
-# Step 2: Calculate total price
-total_price = price_per_unit * quantity
 
-# Step 3: Display result
-print("\nProduct:", product_name)
-print("Price per unit:", price_per_unit)
-print("Quantity:", quantity)
-print("Total Price =", total_price)
   
 
 #----------output---------------------------
-'''Enter a string: Hello World
-Number of vowels in the string: 3
-'''
