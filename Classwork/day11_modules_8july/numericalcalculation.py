3c80d2e15d9aad097468d6cb782f7eedbc930e0d
Priyanshi-coder5 <priyanshi0931@gmail.com>
2026-07-17 12:26:24 +0530

    my sql

---
+++ Classwork/day11_modules_8july/numericalcalculation.py
@@ -0,0 +1,19 @@
+#function to calculation addition of two numbers 
+def calculation_addition(num1, num2):
+    return num1 + num2
+
+#function to calculation subtraction of two numbers 
+def calculation_subtraction(num1, num2):
+    return num1 - num2
+
+#function to calculation multiplication of two numbers 
+def calculation_multiplication(num1, num2):
+    return num1 * num2
+
+#function to calculation division of two numbers 
+def calculation_division(num1, num2):
+
+    return num1 / num2  
+#function to calculation modulus of two numbers
+def calculation_modulus(num1, num2):
+    return num1 % num2
